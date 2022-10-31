8f9e5f6922511524c8bd8e6ac4d6dc576b113d45
mineralaz <jakimaviciuspovilas@gmail.com>
2022-10-31 08:56:04 +0200

    init

---
+++ JS/sort.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log("sort.js");
+
+const users = [
+  {
+    name: "James",
+    age: 25,
+    gender: "male",
+    town: "London",
+  },
+  {
+    name: "Mike",
+    age: 35,
+    gender: "male",
+    town: "Kaunas",
+  },
+  {
+    name: "Jane",
+    age: 27,
+    gender: "female",
+    town: "Kaunas",
+  },
+  {
+    name: "June",
+    age: 47,
+    gender: "female",
+    town: "London",
+  },
+];
